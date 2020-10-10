9993b359084720a030dae3c727fe8cbb22e2dc8e
JHONATHAN <JHONATHAN@DESKTOP-B5GH9H7>
2020-10-10 16:54:51 -0300

    teste

--- aps/CadMiniatura_Jhonathan/src/teste/teste.java
+++ aps/CadMiniatura_Jhonathan/src/teste/teste.java
@@ -3,14 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.com.jhonathan.util;
+package teste;
 
 /**
  *
  * @author JHONATHAN
- * Conexão com o banco
  */
-
-public class Conexao {
+public class teste {
     
 }
