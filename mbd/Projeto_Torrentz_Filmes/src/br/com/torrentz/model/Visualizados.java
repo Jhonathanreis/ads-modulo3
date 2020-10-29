4636d533dba4f602b73398c1f231ad9e850d65f8
JHONATHAN <JHONATHAN@DESKTOP-B5GH9H7>
2020-10-28 22:44:33 -0200

    Classe model, concluida.

--- mbd/Projeto_Torrentz_Filmes/src/br/com/torrentz/model/Visualizados.java
+++ mbd/Projeto_Torrentz_Filmes/src/br/com/torrentz/model/Visualizados.java
@@ -14,10 +14,71 @@
  */
 package br.com.torrentz.model;
 
+import java.sql.Date;
+
 /**
  *
  * @author JHONATHAN
  */
 public class Visualizados {
     
+    //---ATRIBUTOS---//
+    private int vis_iden = 0;
+    private boolean vis_completo = false;
+    private Date vis_data_geracao;
+    private Usuarios vis_usu_iden = null;
+    private Filmes vis_fil_iden = null;
+    
+    //---CONSTRUTOR PADRÃO---//
+    public Visualizados() {
+        
+    }
+    
+    //---CONSTRUTOR PASSANDO PARAMETRO---//
+    public Visualizados(int vis_iden, boolean vis_completo, Date vis_data_geracao) {
+        this.vis_iden = vis_iden;
+        this.vis_completo =vis_completo;
+        this.vis_data_geracao =  vis_data_geracao;
+    }
+    
+    //---GETERS E SETTERS---//
+    public int getVis_iden() {
+        return vis_iden;
+    }
+
+    public void setVis_iden(int vis_iden) {
+        this.vis_iden = vis_iden;
+    }
+
+    public boolean isVis_completo() {
+        return vis_completo;
+    }
+
+    public void setVis_completo(boolean vis_completo) {
+        this.vis_completo = vis_completo;
+    }
+
+    public Date getVis_data_geracao() {
+        return vis_data_geracao;
+    }
+
+    public void setVis_data_geracao(Date vis_data_geracao) {
+        this.vis_data_geracao = vis_data_geracao;
+    }
+
+    public Usuarios getVis_usu_iden() {
+        return vis_usu_iden;
+    }
+
+    public void setVis_usu_iden(Usuarios vis_usu_iden) {
+        this.vis_usu_iden = vis_usu_iden;
+    }
+
+    public Filmes getVis_fil_iden() {
+        return vis_fil_iden;
+    }
+
+    public void setVis_fil_iden(Filmes vis_fil_iden) {
+        this.vis_fil_iden = vis_fil_iden;
+    }
 }
