2dd7935bafade50ff7ad3de5e462825a6d413016
JHONATHAN <JHONATHAN@DESKTOP-B5GH9H7>
2020-10-30 15:24:50 -0300

    Corrigido o JAR

---
+++ pp/Padrao_TemplateMethod_Ordenacao/src/classes/NewClass.java
@@ -0,0 +1,29 @@
+/*
+ *  -------------------------------------------------------------------------------------------------->
+ *  Licença    : MIT - Copyright 2019 Jhonathan dos reis (jhonlinux) <jhonathan.rosa@maximatech.com.br>
+ *  Criado em  : 30/10/2020 15:19:13 
+ *  Instituição: FACULDADE SENAI FATESG
+ *  Curso      : Análise e Desenvolvimento de sistemas - Módulo 3 - 2020/2
+ *  Disciplina : mbd - Modelagem de banco de dados
+ *  Aluno      : Jhonathan dos reis
+ *  Projeto    : PROJETO CAMADAS
+ *  Exercício  : Cadastro de miniaturas
+ *  ---------------------------------------------------------------------------------------------------
+ *  Propósito do arquivo (Objetivo).
+ *  ---------------------------------------------------------------------------------------------------| 
+ */
+
+package classes;
+
+/**
+ *
+ * @author JHONATHAN
+ */
+public class NewClass {
+
+    //---ATRIBUTOS---//
+    //---CONSTRUTOR PADRÃO---//
+    //---CONSTRUTOR PASSANDO PARAMETRO---//
+    //---GETERS E SETTERS---//
+
+}
